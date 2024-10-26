049b5dbdc8b841bb7004f039664049ac532007f2
ammar-ali-cpu <ammaralinyc@gmail.com>
2024-10-26 20:20:16 +0000

    trying my code for load and remove

--- src/image.c
+++ src/image.c
@@ -1,6 +1,6 @@
 #include "image.h"
 
-/*
+
 Image *load_image(char *filename) 
 {    
     FILE *fp = fopen(filename, "r");
@@ -40,62 +40,8 @@ Image *load_image(char *filename)
     fclose(fp);  
     return image;
 }
-*/
 
-Image *load_image(char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        return NULL;
-    }
 
-    char format[3];
-    unsigned short width, height;
-    int max_intensity;
-
-    // Read the PPM header
-    fscanf(file, "%2s", format); // Read "P3"
-    if (strcmp(format, "P3") != 0) {
-        fclose(file);
-        return NULL;
-    }
-
-    // Skip comments if present
-    int c = fgetc(file);
-    while (c == '#') {
-        while (fgetc(file) != '\n');
-        c = fgetc(file);
-    }
-    ungetc(c, file);
-
-    fscanf(file, "%hu %hu", &width, &height);
-    fscanf(file, "%d", &max_intensity);
-
-    // Allocate memory for the image
-    Image *image = malloc(sizeof(Image));
-    if (!image) {
-        fclose(file);
-        return NULL;
-    }
-    image->width = width;
-    image->height = height;
-    image->imageData = malloc(width * height * sizeof(unsigned char));
-    if (!image->imageData) {
-        free(image);
-        fclose(file);
-        return NULL;
-    }
-
-    // Read pixel data
-    for (unsigned int i = 0; i < width * height; i++) {
-        int pixel_value;
-        fscanf(file, "%d", &pixel_value);
-        image->imageData[i] = (unsigned char)pixel_value;
-        fscanf(file, "%*d %*d"); // Skip the other two RGB components
-    }
-
-    fclose(file);
-    return image;
-}
 
 void delete_image(Image *image) 
 {
